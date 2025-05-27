86782af1a8bf697322dddb9a4fb0a20d43399130
RutShiraRabinovitz <Hand1D30@mbjcomp.org.il>
2025-05-27 20:41:47 +0300

    Your commit message here

---
+++ Backend/Dal/Services/StudioServiceService.cs
@@ -0,0 +1,70 @@
+﻿using Dal.Api;
+using Dal.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Services
+{
+    public class StudioServiceService : IStudioService
+    {
+        private readonly DatabaseManager _databaseManager;
+
+        public StudioServiceService(DatabaseManager db)
+        {
+            _databaseManager = db;
+        }
+        public void Add(StudioService entity)
+        {
+            if (entity == null)
+            {
+                Console.WriteLine("item is empty");
+                return;
+            }
+            _databaseManager.StudioServices.Add(entity);
+            _databaseManager.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entity = _databaseManager.StudioServices.FirstOrDefault(e => e.Id == id) ??
+                throw new KeyNotFoundException($"Entity with ID {id} not found.");
+            _databaseManager.StudioServices.Remove(entity);
+            _databaseManager.SaveChanges();
+        }
+
+        public IEnumerable<StudioService> GetAll()
+        {
+            return _databaseManager.StudioServices;
+        }
+
+        public StudioService GetById(int id)
+        {
+            var entity = _databaseManager.StudioServices.FirstOrDefault(e => e.Id == id) ??
+               throw new KeyNotFoundException($"Entity with ID {id} not found.");
+            return entity;
+        }
+
+            public void Update(StudioService entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity), "The entity cannot be null.");
+            }
+
+            var existingEntity = _databaseManager.StudioServices.FirstOrDefault(e => e.Id == entity.Id)
+                ?? throw new KeyNotFoundException($"service with ID {entity.Id} not found.");
+            existingEntity.Id = entity.Id;
+            existingEntity.Price = entity.Price;
+            existingEntity.ServiceName = entity.ServiceName;
+            existingEntity.Description = entity.Description;
+            existingEntity.Duration = entity.Duration;
+
+            _databaseManager.SaveChanges();
+        }
+
+      
+    }
+}
--- Backend/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Backend/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+637e69863bb4df0a295a999afcffdebfd663f866")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238924b11fa1b7ce1cff3794a1b3d4d4efdd885b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
